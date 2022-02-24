1476a45901ea6f0bfcbd57e60eefe91d85f8ca9f
Dmitry Kotik <k-work31@yandex.ru>
2022-02-24 03:35:22 +0300

    lesson12 Попробовал различные варианты сборки

--- app/src/main/java/ru/dkotik/buildvariants/MainActivity.kt
+++ app/src/main/java/ru/dkotik/buildvariants/MainActivity.kt
@@ -2,10 +2,15 @@ package ru.dkotik.buildvariants
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ru.dkotik.buildvariants.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.textView.text = getString(R.string.hello)
     }
 }
